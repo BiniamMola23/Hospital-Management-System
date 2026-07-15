abc48d0c18a8b310d6a0f78db5af6f713256ac0e
Biniam Mola <biniammo12@gmail.com>
2026-07-15 05:51:20 -0700

    update run

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -1,11 +1,18 @@
-import mongoose from "mongoose";
+import mysql from "mysql2";
 
 export const dbConnection = () => {
-    mongoose.connect(process.env.MONGO_URI, {
-        dbName: "Life_Care_Hospital"
-    }).then(() => {
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+
+    connection.connect((err) => {
+        if (err) {
+            console.error(`Error connecting to Database: ${err}`);
+            return;
+        }
         console.log("Connected to Database");
-    }).catch(err => {
-        console.log(`Some error occured while connecting to Database: ${err}`);
-    })
+    });
 }
